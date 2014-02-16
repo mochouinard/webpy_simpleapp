3020d955c0430aaea91224a93f67540ec8138014
Marc Olivier Chouinard <mochouinard@moctel.com>
2014-02-16 14:13:22 -0500

    Reident some lines. Added basic static file provider Added a xml template for the ticket list Added sample jquery/ajax code that query the ticket list.

--- server.py
+++ server.py
@@ -6,17 +6,17 @@ import model
 
 # This is used to simplify getting value from the python ConfigParser
 def ConfigSectionMap(section):
-    dict1 = {}
-    options = Config.options(section)
-    for option in options:
-        try:
-            dict1[option] = Config.get(section, option)
-            if dict1[option] == -1:
-                DebugPrint("skip: %s" % option)
-        except:
-            print("exception on %s!" % option)
-            dict1[option] = None
-    return dict1
+	dict1 = {}
+	options = Config.options(section)
+	for option in options:
+		try:
+			dict1[option] = Config.get(section, option)
+			if dict1[option] == -1:
+				DebugPrint("skip: %s" % option)
+		except:
+			print("exception on %s!" % option)
+			dict1[option] = None
+	return dict1
 
 # Config file stuff
 Config = ConfigParser.ConfigParser()
@@ -39,17 +39,17 @@ db = web.database(dbn='sqlite', db='sample.db')
 
 # Define the different URL regex and their corresponding class
 urls = ('/ticket/(.*)', 'ticket',
-        '/auth/(.*)', 'auth',
+	'/auth/(.*)', 'auth',
 	'/admin/user/(.*)', 'admin_user')
 
 app = web.application(urls, globals())
 
 # This is code to allow session to work while in debug mode
 if web.config.get('_session') is None:
-        session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'email': ""})
-        web.config._session = session
+	session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'email': ""})
+	web.config._session = session
 else:
-        session = web.config._session
+	session = web.config._session
 
 # Load the webpy template engine
 render = web.template.render('templates/', base='layout', globals={'session': session})
@@ -72,32 +72,32 @@ vuser_alreadyverified = form.Validator('Account already verified', lambda u: u i
 # You can see vemail vpass that was define before.  If you use the web UI and try to write an email that not in the correct format, it will fail.. it done because of this
 
 login_form = form.Form(
-    form.Textbox("email", vemail, description="E-Mail"),
-    form.Password("password", vpass, description="Password"),
-    form.Button("submit", type="submit", description="Login"),
-    validators = [
-	vuser_notexist,
-	vuser_auth,
-	vuser_verified]
+	form.Textbox("email", vemail, description="E-Mail"),
+	form.Password("password", vpass, description="Password"),
+	form.Button("submit", type="submit", description="Login"),
+	validators = [
+		vuser_notexist,
+		vuser_auth,
+		vuser_verified]
 )
 
 register_form = form.Form(
-    form.Textbox("email", vemail, description="E-Mail"),
-    form.Password("password", vpass, description="Password"),
-    form.Password("password2", description="Repeat password"),
-    form.Button("submit", type="submit", description="Register"),
-    validators = [
-        form.Validator("Passwords didn't match", lambda i: i.password == i.password2),
-	vuser_exist]
+	form.Textbox("email", vemail, description="E-Mail"),
+	form.Password("password", vpass, description="Password"),
+	form.Password("password2", description="Repeat password"),
+	form.Button("submit", type="submit", description="Register"),
+	validators = [
+		form.Validator("Passwords didn't match", lambda i: i.password == i.password2),
+		vuser_exist]
 )
 
 authkey_form = form.Form(
 	form.Textbox("email", vemail, description="E-Mail"),
 	form.Button("submit", type="submit", description="Request Key"),
 	validators = [
-		vuser_notexist,
-		vuser_alreadyverified,
-		vuser_notverified]
+			vuser_notexist,
+			vuser_alreadyverified,
+			vuser_notverified]
 )
 
 class admin_user:
@@ -121,7 +121,7 @@ class auth:
 			f = register_form()
 			return render.auth_create_get(f)
 		elif rest == 'authkey':
-                        f = authkey_form()
+			f = authkey_form()
 			i = web.input(key='')
 			if i.key != "":
 				res = model.check_auth_string(db,i.key)
@@ -133,14 +133,14 @@ class auth:
 					return render.auth_authkey_res_KEY_INVALID()
 				else:
 					return web.internalerror("Invalid response")
-                        return render.auth_authkey_get(f)
+			return render.auth_authkey_get(f)
 
-	        return "TODO"
+		return "TODO"
 	def POST(self, rest):
 		if rest == 'login':
-                        f = login_form()
-                        if not f.validates():
-                                return render.auth_login_get(f)
+			f = login_form()
+			if not f.validates():
+				return render.auth_login_get(f)
 			# TODO Save User Session Authentication
 			i = web.input()
 			session.email = i.email
@@ -173,16 +173,42 @@ class auth:
 			
 # A just a place holder for a ticket system, doesn't do much
 class ticket:
-    def GET(self, name):
-	tickets = db.select('tickets')
-	return render.index_get(tickets)
-    def POST(self, name=None):
-	i = web.input()
-	id = db.insert("tickets", title=i.title)
+	def GET(self, name):
+		web.header('Access-Control-Allow-Origin', '*')
 
-	return render.index(id)
-	#return "ADDED %s" % (name,)
+		tickets = db.select('tickets')
+		return render.index_get(tickets)
+	def POST(self, name=None):
+		i = web.input()
+		id = db.insert("tickets", title=i.title)
 
+		return render.index(id)
+		#return "ADDED %s" % (name,)
+	def OPTIONS(Self, name=None):
+		web.header('Access-Control-Allow-Origin', '*')
+		web.header('Access-Control-Allow-Headers', '*')
+		web.header('Access-Control-Max-Age', '1728000')
+		web.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, PUT, OPTIONS')
+		web.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+		return ""
+
+class static:
+	def GET(self,name):
+		ext = name.split(".")[-1] # Gather extension
+
+		cType = {
+				"png":"images/png",
+				"jpg":"images/jpeg",
+				"gif":"images/gif",
+				"ico":"images/x-icon"  
+		}
+
+		if name in os.listdir('static'):  # Security
+				if cType[ext]:
+					web.header("Content-Type", cType[ext]) # Set the Header
+				return open('static/%s'%name,"rb").read() # Notice 'rb' for reading images
+		else:
+				raise web.notfound()
 
 if __name__ == "__main__":
 	app.run()
