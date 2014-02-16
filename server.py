8b4ef0bc95d22647c9b8f8ce7fa470bdc366f0d7
Marc Olivier Chouinard <mochouinard@moctel.com>
2014-02-16 10:23:42 -0500

    Some code Comments plus placeholder for admin class

--- server.py
+++ server.py
@@ -1,11 +1,10 @@
+# Sample code of a more complex webpy application: https://github.com/nopri/onlinestore-multi
 import web
 from web import form
 import ConfigParser
-
-#res = model.check_auth_string(db,i.key)
-# Sample code https://github.com/nopri/onlinestore-multi
 import model
 
+# This is used to simplify getting value from the python ConfigParser
 def ConfigSectionMap(section):
     dict1 = {}
     options = Config.options(section)
@@ -19,34 +18,43 @@ def ConfigSectionMap(section):
             dict1[option] = None
     return dict1
 
+# Config file stuff
 Config = ConfigParser.ConfigParser()
 Config.read('local.ini')
-
-
-urls = ('/ticket/(.*)', 'ticket',
-	'/auth/(.*)', 'auth')
 
 web.config.smtp_server = ConfigSectionMap("smtp")['server']
 web.config.smtp_username = ConfigSectionMap("smtp")['username']
 web.config.smtp_password = ConfigSectionMap("smtp")['password']
+web.config.smtp_debuglevel = 99
+smtp_starttls = ConfigSectionMap("smtp")['starttls']
+if smtp_starttls == "true":
+	web.config.smtp_starttls = True
 web.config._from_email_automated = ConfigSectionMap("smtp")['from']
-
 web.config._site_root = ConfigSectionMap("web")['root_url']
 
+# Database initiation for the webpy engine
 db = web.database(dbn='sqlite', db='sample.db')
 
 #web.config.debug = False
 
+# Define the different URL regex and their corresponding class
+urls = ('/ticket/(.*)', 'ticket',
+        '/auth/(.*)', 'auth',
+	'/admin/user/(.*)', 'admin_user')
+
 app = web.application(urls, globals())
+
+# This is code to allow session to work while in debug mode
 if web.config.get('_session') is None:
         session = web.session.Session(app, web.session.DiskStore('sessions'), initializer={'email': ""})
         web.config._session = session
 else:
         session = web.config._session
 
+# Load the webpy template engine
 render = web.template.render('templates/', base='layout', globals={'session': session})
 
-
+# This are Form validation value.  We define them here because we can reuse them in different forms
 vpass = form.regexp(r".{3,20}$", 'must be between 3 and 20 characters')
 vemail = form.regexp(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', "must be a valid email address")
 vuser_exist = form.Validator('Username already exist. <a href="/auth/login">Click here</a> to get to the login screen or <a href="/auth/lost">click here</a> if you lost your password.', lambda u: u is None or model.get_user(db,u.email) is None)
@@ -59,6 +67,9 @@ vuser_verified = form.Validator('Account not validated. <a href="/auth/authkey">
 vuser_notverified = form.Validator('Account is not waiting to be verified', lambda u: u is not None and model.get_user(db,u.email).status == 'VERIFICATION_REQUIRED')
 
 vuser_alreadyverified = form.Validator('Account already verified', lambda u: u is not None and model.get_user(db,u.email).status != 'VERIFIED')
+
+# This is where we define form to be generated in HTML by webpy.  It include support for fields verifications.
+# You can see vemail vpass that was define before.  If you use the web UI and try to write an email that not in the correct format, it will fail.. it done because of this
 
 login_form = form.Form(
     form.Textbox("email", vemail, description="E-Mail"),
@@ -89,8 +100,15 @@ authkey_form = form.Form(
 		vuser_notverified]
 )
 
-
+class admin_user:
+	def GET(self, rest):
+		return "Requested data for %s" % ( rest,)
+	def DELETE(self, rest):
+		return "%s DELETED" % (rest)
+	
+# This is the main class auth that is called used by the urls value we saw before
 class auth:
+	# This is the type of query... We got GET POST PUT DELETE (May other, but I know of those)
 	def GET(self, rest):
 		if rest == 'logout':
 			session.kill()
@@ -153,7 +171,7 @@ class auth:
 			
 
 			
-
+# A just a place holder for a ticket system, doesn't do much
 class ticket:
     def GET(self, name):
 	tickets = db.select('tickets')
